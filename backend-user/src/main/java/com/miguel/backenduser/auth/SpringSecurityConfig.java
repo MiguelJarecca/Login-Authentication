387620b3d502d68cc88d7fd4f348431d421937b8
miguel <miguel.jarecca@gmail.com>
2024-01-25 18:52:33 -0500

    Se implementa userDetailsService

--- backend-user/src/main/java/com/miguel/backenduser/auth/SpringSecurityConfig.java
+++ backend-user/src/main/java/com/miguel/backenduser/auth/SpringSecurityConfig.java
@@ -1,14 +1,28 @@
 package com.miguel.backenduser.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.miguel.backenduser.auth.filters.JwtAuthFilter;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
     
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -16,6 +30,8 @@ public class SpringSecurityConfig {
         return http.authorizeHttpRequests(andRules -> andRules
             .requestMatchers(HttpMethod.GET, "/users").permitAll()
             .anyRequest().authenticated())
+
+            .addFilter(new JwtAuthFilter(authenticationConfiguration.getAuthenticationManager()))
             
             .csrf(config -> config.disable())
             .sessionManagement(managment -> 
